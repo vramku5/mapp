d1b46873f6c0dca6f2658a9b9ae5d3e7f42e69c2
Ramkumar V <vramku@gmail.com>
2015-05-04 20:54:57 +0530

    Updated with springboot

--- springrest/src/main/java/com/vramku/springrest/common/controller/MovieController.java
+++ springrest/src/main/java/com/vramku/springrest/common/controller/MovieController.java
@@ -1,29 +1,50 @@
 package com.vramku.springrest.common.controller;
- 
+
+import java.util.Date;
+
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+
+import com.vramku.springrest.user.DBUser;
+import com.vramku.springrest.util.HibernateUtil;
+
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
- 
+
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String getMovie(@PathVariable String name, ModelMap model) {
- 
+
 		model.addAttribute("movie", name);
 		return "list";
- 
+
 	}
- 
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getDefaultMovie(ModelMap model) {
- 
+
 		model.addAttribute("movie", "this is default movie");
 		return "list";
- 
+
 	}
- 
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void createMovie(@PathVariable String name) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		DBUser user = new DBUser();
+
+		user.setUserId(100);
+		user.setUsername("superman");
+		user.setCreatedBy("system");
+		user.setCreatedDate(new Date());
+
+		session.save(user);
+		session.getTransaction().commit();
+	}
 }
